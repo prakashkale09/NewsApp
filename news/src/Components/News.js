66e566ff79663e96ac54f263a2c1df4d1c5687fa
prakashkale09 <prakashkale202@gmail.com>
2024-02-15 09:42:22 +0530

    Author name ,Published date & time as well as sources are added sucessfully

--- news/src/Components/News.js
+++ news/src/Components/News.js
@@ -92,7 +92,7 @@ export class News extends Component {
         {!this.state.loading && this.state.articles.map((element)=>
         {
             return <div className="col-md-4"key ={ element.url }>
-            <NewsItem title={element.title?element.title.slice(0,45):""} description =  {element.description?element.description.slice(0,80):""}imageUrl= {element.urlToImage} Newsurl={element.url}/>
+            <NewsItem title={element.title?element.title.slice(0,45):""} description =  {element.description?element.description.slice(0,80):""}imageUrl= {element.urlToImage} Newsurl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
             </div>
         })}    
       </div>
--- news/src/Components/NewsItem.js
+++ news/src/Components/NewsItem.js
@@ -3,15 +3,18 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     
   render() {
-    let {title,description,imageUrl,Newsurl} = this.props;
+    let {title,description,imageUrl,Newsurl,author,date,source} = this.props;
     return (
       <div className='my-3'>
-       <div className="card" style={{width: "18rem"}}>
+       <div className="card"  >
+       <span class="position-absolute top-0   translate-middle badge rounded-pill bg-danger"style={{left:"50%", zIndex:"65"}}>{source}
+  </span>
   <img src= {imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
+    <h5 className="card-title">{title}  </h5>
     <p className="card-text"> {description}...</p>
-    <a href={Newsurl} target="" className="btn btn-sm btn-primary">Read more...</a>
+    <p className='card-text'><small class ="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+    <a rel="noreferrer"href={Newsurl} target="_blank" className="btn btn-sm btn-primary">Read more...</a>
   </div>
 </div>
       </div>
